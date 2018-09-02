f7f2c63371ff9851d693272c2a77b9f13bb5a5ba
Luis Lopez <l_y02@hotmail.com>
2018-09-01 19:25:31 -0500

    Add Splash Screen and Lottie

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import LottieView from 'lottie-react-native';
-import { StyleSheet, Text, View } from 'react-native';
-import {
-  createStackNavigator,
-} from 'react-navigation';
+import { StyleSheet, Text, View, Button, Image, ImageBackground } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  
+
+class HomeScreen extends React.Component {
+
+  render() {
+    setInterval(() => {
+        this.props.navigation.navigate('Presentation');
+    }, 5000);
+    return (
+        <ImageBackground style={styles.containerSplash} source={require('./assets/fondo.png')}>
+          <Image source={require('./assets/Group.png')}>
+          </Image>
+        </ImageBackground>
+    );
+  }
+}
+
+class PresentationScreen extends React.Component {
   componentDidMount() {
     this.animation.play();
     // Or set a specific startFrame and endFrame with:
@@ -15,15 +28,51 @@ export default class App extends React.Component {
 
   render() {
     return (
+    <View style={{ flex: 1 }}>
       <LottieView
         ref={animation => {
           this.animation = animation;
         }}
         source={require('./assets/servishero_loading.json')}
       />
+        <Button
+            title="Nuevo Xpense"
+            onPress={() => this.props.navigation.navigate('User')}
+          />
+      </View>
     );
   }
+}
 
+class UserScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>User Screen</Text>
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Presentation: PresentationScreen,
+    User: UserScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
 }
 
 const styles = StyleSheet.create({
@@ -33,4 +82,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerSplash: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
